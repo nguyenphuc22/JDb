dfa08e05f85f23812ac0ad293b2da5c31117d638
nguyenphuc22 <59957741+nguyenphuc22@users.noreply.github.com>
2022-01-03 16:25:59 +0700

    Revert "fix join Table"
    This reverts commit 9c5973e31f746769e77c74555f10e268dc372a81.

---
+++ src/DATABASE/JDBTest.java
@@ -0,0 +1,82 @@
+package DATABASE;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JDBTest {
+
+    @BeforeEach
+    void SetUp() {
+        System.out.println("Before");
+    }
+
+    @AfterEach()
+    void TearDown() {
+        System.out.println("After");
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void open() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createTable() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void insert() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testInsert() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void delete() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testDelete() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testDelete1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testDelete2() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void update() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testUpdate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testUpdate1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testUpdate2() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void close() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void get() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGet() {
+    }
+}
